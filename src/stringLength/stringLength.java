700de7a12494280e6e98054cb52d26b44a1d039a
Megan Majewski <megan.majewski@yahoo.com>
2016-06-09 14:15:42 -0400

    string length java

---
+++ src/stringLength/stringLength.java
@@ -0,0 +1,28 @@
+/* this program will accept input from the user and determine which word is the longest
+ * TODO: Add logic to ignore punctuation
+ * 
+ */
+
+package stringLength;
+
+import java.util.Scanner;
+
+public class stringLength {
+	public static String longestWord(String sen){
+		String[] wordsArr= sen.split(" ");
+		int test=0;
+		for(int i=0; i<wordsArr.length; i++){
+			if(wordsArr[i].length()>test){
+				test=wordsArr[i].length();
+				sen=wordsArr[i];
+			}
+		}
+		return sen;
+	}
+	public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	System.out.println("Enter a sentence to find the longest word");
+	System.out.print(longestWord(s.nextLine()));
+	}
+
+}
